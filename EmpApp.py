827dfaa434e3ea1b298d23fe12f210040cd0003d
Madhukotipalli <75793969+Madhukotipalli@users.noreply.github.com>
2021-08-29 13:15:44 +0530

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -83,10 +83,10 @@ def AddEmp():
             try:
                 dynamodb_client = boto3.client('dynamodb', region_name='us-east-2')
                 dynamodb_client.put_item(
-                 TableName='employee_image_table',
+                 TableName='users_image_table',
                     Item={
-                     'empid': {
-                          'N': emp_id
+                     'userid': {
+                          'N': user_id
                       },
                       'image_url': {
                             'S': object_url
